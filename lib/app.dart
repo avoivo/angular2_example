925aeac8314aa0e09b86cb901f075c998d0f009e
Thanasis Voivodas <thanasis.voivodas@taxydromiki.gr>
2015-11-13 13:15:20 +0200

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+library app;
+
+import 'package:angular2/angular2.dart';
+import 'package:angular2/bootstrap.dart';
+
+part 'components/main_component.dart';
+
+void main(){
+  bootstrap(MainComponent);
+}
+
---
+++ lib/components/main_component.dart
@@ -0,0 +1,12 @@
+part of app;
+
+@Component(
+  selector: "main"
+)
+@View(
+  templateUrl: "package:angular2_example/components/main_component.html",
+  styleUrls: const ["package:angular2_example/components/main_component.css"]
+)
+class MainComponent{
+  
+}
